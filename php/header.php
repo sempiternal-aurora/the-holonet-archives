d856bd6aa5a2efb4786cd705b2ec59b0db02ef24
Shadowstone12 <myrialsarvay@gmail.com>
2022-05-09 23:01:19 +1000

    Update header to add privilege detection
    adds privilege detection to the header, hiding the modify unit
    button when user is not a member of Shop Review
    Also changed buttons over to new Jquery format

--- php/header.php
+++ php/header.php
@@ -30,6 +30,8 @@
     //Check whether the user is logged in, and which account it is done from
     list($user, $userstr, $logged_in, $logged_in_as) = check_user_status();
 
+    $privilege = $logged_in ? check_user_privilege($pdo, $user) : [];
+
     //using the information from above, finish the header of the document with a personalised title, and start the body section with the logo
     echo <<<_ECHO
             <title>The Holonet Archives$userstr</title>
@@ -49,13 +51,17 @@
 
     
     if ($logged_in) {//If they are logged in, display a button linking to the home, modify unit, order calculator, unit search, profile and log out page
+        echo "<div class='centre'>";
+
+        if (in_array(3, $privilege)) {
+            echo "<a data-role='button' data-inline='true' data-icon='edit' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/modify_unit?r=$randstr'>Modify Unit</a>";
+        }
+
         echo <<<_ECHO
-                    <div class='centre'>
-                        <a data-role='button' data-inline='true' data-icon='edit' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/modify_unit?r=$randstr'>Modify Unit</a>
-                        <a class='ui-btn ui-btn-inline ui-icon-tag ui-btn-icon-left' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/shop_page?r=$randstr'>All Shops</a>
-                        <a class='ui-btn ui-btn-inline ui-icon-bullets ui-btn-icon-left' data-transtition='slidefade' data-ajax='false' href='{$C('WEBSITE_ROOT')}/unit_lookup?r=$randstr'>Unit Search</a>
-                        <a class='ui-btn ui-btn-inline ui-icon-action ui-btn-icon-left' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/profile/log_out.php?r=$randstr'>Log out</a>
-                        <a class='ui-btn ui-btn-inline ui-icon-comment ui-btn-icon-left' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/help/?r=$randstr'>Help</a>
+                        <a data-role='button' data-inline='true' data-icon='tag' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/shop_page?r=$randstr'>All Shops</a>
+                        <a data-role='button' data-inline='true' data-icon='bullets' data-transtition='slidefade' data-ajax='false' href='{$C('WEBSITE_ROOT')}/unit_lookup?r=$randstr'>Unit Search</a>
+                        <a data-role='button' data-inline='true' data-icon='action' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/profile/log_out.php?r=$randstr'>Log out</a>
+                        <a data-role='button' data-inline='true' data-icon='comment' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/help/?r=$randstr'>Help</a>
                     </div>
         _ECHO;
     }
@@ -63,10 +69,10 @@
         echo <<<_ECHO
                     <div class='centre'>
                         <a data-role='button' data-inline='true' data-icon='edit' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/shop_page?r=$randstr'>All Shops</a>
-                        <a class='ui-btn ui-btn-inline ui-icon-bullets ui-btn-icon-left' data-transtition='slidefade' data-ajax='false' href='{$C('WEBSITE_ROOT')}/unit_lookup?r=$randstr'>Unit Search</a>
-                        <a class='ui-btn ui-btn-inline ui-icon-plus ui-btn-icon-left' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/profile/sign_up.php?r=$randstr'>Sign Up</a>
-                        <a class='ui-btn ui-btn-inline ui-icon-check ui-btn-icon-left' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/profile/login.php?r=$randstr'>Login</a>
-                        <a class='ui-btn ui-btn-inline ui-icon-comment ui-btn-icon-left' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/help/?r=$randstr'>Help</a>
+                        <a data-role='button' data-inline='true' data-icon='bullets' data-transtition='slidefade' data-ajax='false' href='{$C('WEBSITE_ROOT')}/unit_lookup?r=$randstr'>Unit Search</a>
+                        <a data-role='button' data-inline='true' data-icon='plus' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/profile/sign_up.php?r=$randstr'>Sign Up</a>
+                        <a data-role='button' data-inline='true' data-icon='check' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/profile/login.php?r=$randstr'>Login</a>
+                        <a data-role='button' data-inline='true' data-icon='comment' data-transtition='slidefade' href='{$C('WEBSITE_ROOT')}/help/?r=$randstr'>Help</a>
                     </div>
         _ECHO;
     }
