b851cb3c963b0caaa5fbcae1f4d7f3c1ec6ad191
Shadowstone12 <myrialsarvay@gmail.com>
2022-05-14 22:54:06 +1000

    Added contact page

---
+++ public_html/contact/index.php
@@ -0,0 +1,15 @@
+<?php
+    require_once '../../php/functions.php';
+    require_once DOCUMENT_ROOT . 'php/header.php';
+
+    echo <<<_END
+        <div class='centre-div'>
+        You can contact the administrator of this website through the following email:
+
+        <div class='centre'><a href='mailto:myrialsarvay@gmail.com'>myrialsarvay@gmail.com</a></div>
+        </div>
+    _END;
+
+    echo "</div>";
+
+    include_once DOCUMENT_ROOT . 'php/footer.php';
