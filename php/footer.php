a5550063e61b43d6bc039acfdbce44a6994881f7
Shadowstone12 <myrialsarvay@gmail.com>
2022-05-14 22:53:50 +1000

    Added link to contact page in footer

--- php/footer.php
+++ php/footer.php
@@ -3,7 +3,7 @@
 
     echo <<<_END
                 <div data-role='footer'>
-                    <h4 class='centre'>Web App Developed by Myria</h4>
+                    <h4 class='centre'>Web App Developed by Myria: <a href='{$C('WEBSITE_ROOT')}/contact?r=$randstr'>Contact me</a></h4>
                     <div class='centre'>By using this website, you agree to the terms of service listed here: <a href='{$C('WEBSITE_ROOT')}/terms_of_use'>{$C('WEBSITE_ROOT')}/terms_of_use</a>
                 </div>
             </div>
