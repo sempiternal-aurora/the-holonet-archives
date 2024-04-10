524d8bde39c71b2edaac6fc777b04001e342f691
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2024-04-10 19:50:05 +1000

    Add link tree to modify unit module

--- public_html/modify_unit/index.php
+++ public_html/modify_unit/index.php
@@ -3,8 +3,19 @@
     require_once DOCUMENT_ROOT . 'php/header.php';
 
     if (!$logged_in) { //If the user is not logged in, display the message below.
-        echo "You must be logged in to use this feature.";
+        echo "You must be logged in to use this feature.</div>";
+    } elseif (!in_array(3, $privilege)) {
+        echo "You do not have the required privilege to use this feature.</div>";
+    } else {
+        echo "<div class='centre centre-div'>";
+        echo "<h2>Please choose an option below</h2>";
+
+        echo "<a data-role='button' data-transtition='slidefade' href='update_unit.php'>Update Unit</a>";
+        echo "<a data-role='button' data-transtition='slidefade' href='update_shop.php'>Update Shop</a>";
+        echo "<a data-role='button' data-transtition='slidefade' href='" . WEBSITE_ROOT . "/modify_unit/add_unit.php'>Add Unit</a>";
+        echo "<a data-role='button' data-transtition='slidefade' href='" . WEBSITE_ROOT . "/modify_unit/add_shop.php'>Add Shop</a>";
+
+        echo "</div></div>";
     }
-    else {
-        
-    }
+
+    include_once DOCUMENT_ROOT . 'php/footer.php';
