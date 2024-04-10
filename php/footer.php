edbeeb76f00f7b473a97ebb2e44ac19b51c9bdd3
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2024-04-10 19:50:05 +1000

    Added terms of use

--- php/footer.php
+++ php/footer.php
@@ -4,6 +4,7 @@
     echo <<<_END
                 <div data-role='footer'>
                     <h4 class='centre'>Web App Developed by Myria</h4>
+                    <div class='centre'>By using this website, you agree to the terms of service listed here: <a href='{$C('WEBSITE_ROOT')}/terms_of_use'>{$C('WEBSITE_ROOT')}/terms_of_use</a>
                 </div>
             </div>
         </body>
