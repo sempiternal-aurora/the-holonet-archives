e220100e48ad94cf1a37ef621e5b210d8047cb9e
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2024-04-10 19:50:05 +1000

    Create placeholder modules for modify unit
    Create add_unit, update_shop and update unit as placeholders for
    modify unit modules

---
+++ public_html/modify_unit/update_shop.php
@@ -0,0 +1,15 @@
+<?php
+    require_once '../../php/functions.php';
+    require_once DOCUMENT_ROOT . 'php/header.php';
+
+    if (!$logged_in) { //If the user is not logged in, display the message below.
+        echo "You must be logged in to use this feature.</div>";
+    } elseif (!in_array(3, $privilege)) {
+        echo "You do not have the required privilege to use this feature.</div>";
+    } else {
+        echo "<div class='centre centre-div'>";
+
+        echo "</div></div>";
+    }
+
+    include_once DOCUMENT_ROOT . 'php/footer.php';
