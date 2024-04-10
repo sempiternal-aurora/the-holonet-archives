4f9b2fab1fd3603c9394e9a3a7c1f4cee1bc15eb
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2024-04-10 19:50:05 +1000

    fix a bug in signup
    Fix a bug in signup module where usernames would
    not actually be added to the table with their hashes

--- public_html/_/js/javascript.js
+++ public_html/_/js/javascript.js
@@ -1,11 +1,11 @@
-function validateUsername(field) {
+function validate_username(field) {
     if (field == "") return "No Username was entered.\n";
     else if (field.length < 4) return "Usernames must be at least 4 characters.\n";
     else if (/[^a-zA-Z0-9_]/.test(field)) return "Only a-z, A-Z, 0-9 and _ allowed in Usernames.\n";
     else return "";
 }
 
-function validatePassword(field) {
+function validate_password(field) {
     if (field == "") return "No Password was entered.\n";
     else if (field.length < 8) return "Passwords must be at least 8 characters\n";
     else if (!/[a-z]/.test(field) || !/[A-Z]/.test(field) || !/[0-9]/.test(field)) {
@@ -15,9 +15,26 @@ function validatePassword(field) {
 }
 
 function validate_login(form) {
-    fail = validateUsername(form.user.value)
-    fail += validatePassword(form.pass.value)
+    fail = validate_username(form.user.value)
+    fail += validate_password(form.pass.value)
 
     if (fail == "") return true
-    else { alert(fail); return false }
+    else { $('.error').text(fail); return false }
 }
+
+function check_user(user) {
+    if (user.value == '') {
+        $('#used').html('&nbsp;')
+        return
+    }
+
+    $.post(
+        'checkuser.php', 
+        { user : user.value }, 
+        function(data) {
+            $('#used').html(data)
+        }
+    )
+}
+
+
